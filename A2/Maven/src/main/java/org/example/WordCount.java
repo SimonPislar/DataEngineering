a020c85f21a5a469b38a592f49925623d242b958
SimonPislar <51m0np15l3r@gmail.com>
2024-02-13 12:24:56 +0100

    Add maven to project, new copy of WordCount.java

--- A2/Maven/src/main/java/org/example/WordCount.java
+++ A2/Maven/src/main/java/org/example/WordCount.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-import java.util.StringTokenizer;
+package org.example;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -10,6 +9,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class WordCount {
 
