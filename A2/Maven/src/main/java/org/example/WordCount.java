6b7a004947a5c1dbf5068e44b78dfd726be9ce64
SimonPislar <51m0np15l3r@gmail.com>
2024-02-13 15:44:17 +0100

    Reworked WordCount.java to count words starting with same character

--- A2/Maven/src/main/java/org/example/WordCount.java
+++ A2/Maven/src/main/java/org/example/WordCount.java
@@ -15,16 +15,22 @@ import java.util.StringTokenizer;
 
 public class WordCount {
 
+    /*
+        @param Object key: The key of the input text
+        @param Text value: The value of the input text
+        @param Context context: The context of the input text
+        @Brief: The map function takes the input text and tokenizes it into words. It then writes the word and the value 1 to the context.
+     */
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
 
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
+            StringTokenizer itr = new StringTokenizer(value.toString()); // Initialize StringTokenizer with the input text.toString()
+            while (itr.hasMoreTokens()) { // While there are more tokens in the input text
+                word.set(String.valueOf(itr.nextToken().toLowerCase().charAt(0))); // Set the word to the next token (word found in the input text)
+                context.write(word, one); // Write the word and the value 1 to the context
             }
         }
     }
